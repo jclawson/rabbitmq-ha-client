3d606db6cb09b9a110ee5588b408a6f3c2a8aa60
Jason Clawson <jason.clawson@gmail.com>
2013-10-02 09:29:25 -0600

    fix for failing preconditions on queue_declare with differing params

--- src/main/java/com/jasonclawson/HaUtils.java
+++ src/main/java/com/jasonclawson/HaUtils.java
@@ -6,8 +6,11 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.ConnectException;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.ConsumerCancelledException;
 import com.rabbitmq.client.ShutdownSignalException;
+import com.rabbitmq.client.impl.AMQCommand;
+import com.rabbitmq.client.impl.AMQImpl.Channel;
 
 
 public final class HaUtils {
@@ -23,8 +26,22 @@ public final class HaUtils {
     	}
     	
     	if(e instanceof ShutdownSignalException) {
+    		Object reason = ((ShutdownSignalException) e).getReason();
+    		if(reason instanceof AMQCommand) {
+    			com.rabbitmq.client.Method method = ((AMQCommand) reason).getMethod();
+    			if(method instanceof Channel.Close) {
+    				if(((Channel.Close) method).getReplyCode() == AMQP.PRECONDITION_FAILED) {
+    					//this is the case when we declare a queue with different params than the one that exists
+    					//TODO: have a configuration option to ignore the erroring method call and continue on
+    					//      if the queue exists... who cares?
+    					return false;
+    				}
+    			}
+    		}
+    		
     		/*
     		 * If we didn't initiate the shutdown, or we encounter a connection error... we should reconnect
+    		 * isHardError returns true if it was a connection error... false if a channel error
     		 */
     		return !((ShutdownSignalException) e).isInitiatedByApplication()
     			|| ((ShutdownSignalException)e).isHardError();
